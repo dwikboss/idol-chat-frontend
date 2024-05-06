ec0343306dc9a6c7e995550d6abc1f1b8a55522a
Dwiky van Bosstraten <dwikyvanbosstraten@livewallgroup.com>
2024-05-06 23:52:23 +0200

    feat: add chat api call to backend

---
+++ src/components/ChatMessage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="messageClass">
+    <div class="message-wrapper">
+      <!-- <p>{{ chat.message }}</p> -->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+// import type { Message } from '../interfaces/_IMessage';
+
+export default defineComponent({
+  name: 'ChatMessage',
+  props: {
+    // chat: {
+    //   type: Object as () => Message,
+    //   required: true,
+    // },
+  },
+  computed: {
+    messageClass(): string {
+      return this.chat.user ? 'chat-message user' : 'chat-message assistant';
+    },
+  },
+});
+</script>
