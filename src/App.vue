7c2a0f13abff1f23a4691788555afea71783594c
Dwiky van Bosstraten <dwikyvanbosstraten@livewallgroup.com>
2024-05-08 02:43:19 +0200

    style: resizing issues

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,18 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+export default defineComponent({
+  mounted() {
+    window.addEventListener('resize', this.appHeight);
+    this.appHeight();
+  },
+  methods: {
+    appHeight() {
+      const doc = document.documentElement;
+      doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+    },
+  },
+});
 </script>
 
 <style lang="scss">
@@ -18,5 +29,19 @@ export default defineComponent({});
 .page {
   min-height: 100vh;
   height: 100%;
+}
+
+:root {
+  --app-height: 100%;
+}
+
+html,
+body {
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+  width: 100vw;
+  height: 100vh;
+  height: var(--app-height);
 }
 </style>
