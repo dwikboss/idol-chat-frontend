1fca187de1cdfde574d38edf6b393142a1df2c75
Dwiky van Bosstraten <dwikyvanbosstraten@livewallgroup.com>
2024-05-08 02:59:07 +0200

    revert: window height on app

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,7 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  mounted() {
-    window.addEventListener('resize', this.appHeight);
-    this.appHeight();
-  },
-  methods: {
-    appHeight() {
-      const doc = document.documentElement;
-      doc.style.setProperty('--app-height', `${window.innerHeight}px`);
-    },
-  },
+
 });
 </script>
 
@@ -29,19 +20,5 @@ export default defineComponent({
 .page {
   min-height: 100vh;
   height: 100%;
-}
-
-:root {
-  --app-height: 100%;
-}
-
-html,
-body {
-  padding: 0;
-  margin: 0;
-  overflow: hidden;
-  width: 100vw;
-  height: 100vh;
-  height: var(--app-height);
 }
 </style>
