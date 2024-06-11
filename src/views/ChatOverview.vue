996bdf97dd224b1c1141876644298b850cffd1e5
dwikboss <dwikboss99@gmail.com>
2024-06-11 18:58:54 +0200

    feat: new chatoverview styling

--- src/views/ChatOverview.vue
+++ src/views/ChatOverview.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="page overview">
+    <div class="dot-overlay"></div>
     <div class="overview-header">
       <div class="full-width">
-        <h2>Messages</h2>
+        <h2><span style="font-family: Helvetica, sans-serif;font-weight: 500;">M</span><span style="font-family: Briem Hand, cursive;font-weight: 200;font-style: italic;">e</span><span style="font-family: Helvetica, sans-serif;font-weight: 700;margin-left: 6px;">S</span><span style="font-family: Helvetica, sans-serif;font-weight: 200;">S</span><span style="font-family: Lora, serif;font-weight: 500;">a</span><span style="font-family: Helvetica, sans-serif;font-weight: 500;">g</span><span style="font-family: Briem Hand, cursive;font-weight: 500;">e</span><span style="font-family: Helvetica, sans-serif;font-weight: 500;">s</span></h2>
       </div>
     </div>
     <div class="full-width">
@@ -39,8 +40,20 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .page.overview {
-  background-color: #fff2fc;
+  background: rgb(118, 138, 195);
+  background: linear-gradient(180deg, rgba(118, 138, 195, 1) 0%, rgba(255, 255, 255, 1) 100%);
   height: 100vh;
+
+  .dot-overlay {
+    background-image: radial-gradient(#ffffff 1.5px, transparent 1.5px);
+    background-size: 18px 18px;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0.4;
+  }
 
   .overview-header {
     height: 18vh;
@@ -52,6 +65,10 @@ export default defineComponent({
 
       h2 {
         margin-bottom: 25px;
+        font-family: "Micro 5 Charted", sans-serif;
+        font-size: 3rem;
+        font-weight: 100;
+        color: white;
       }
     }
   }
