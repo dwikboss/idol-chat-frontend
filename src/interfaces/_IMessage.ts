1166db34f0d605bd1ab5b454c659a465ca9e53a3
dwikboss <dwikboss99@gmail.com>
2025-07-23 16:05:38 +0200

    fix: update interface

--- src/interfaces/_IMessage.ts
+++ src/interfaces/_IMessage.ts
@@ -1,6 +1,9 @@
 export default interface Message {
   role: string;
-  content: string;
+  content: string | Array<
+    | { type: 'text'; text: string }
+    | { type: 'image_url'; image_url: { url: string } }
+  >;
   content_KR?: string;
   media?: string;
   grouped_message?: boolean;
